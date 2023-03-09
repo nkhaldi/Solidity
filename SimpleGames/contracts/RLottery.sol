f1c29a1bfe963e076aa26e77a721d5bb0cfeabc4
nkhaldi <mnk80898@gmail.com>
2023-03-09 21:48:45 +0300

    added RLottery

---
+++ SimpleGames/contracts/RLottery.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract RLottery {
+  uint maxNum = 50;
+  struct player {
+    uint ticket;
+  }
+
+  mapping (uint => player) public players;
+
+  function getTicket() public payable returns(string memory, uint) {
+    uint newTicket;
+    do {
+      newTicket = block.timestamp % maxNum;
+    } while (players[newTicket].ticket > 0);
+
+    players[newTicket] = player(newTicket);
+    return("Your ticket", newTicket);
+  }
+
+  function winner() public view returns(string memory, uint) {
+    uint winTicket;
+    do {
+      winTicket = block.timestamp % maxNum;
+    } while (players[winTicket].ticket == 0);
+
+    return("Winner", winTicket);
+  }
+}
