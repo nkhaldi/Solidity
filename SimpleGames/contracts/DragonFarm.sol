7318fa8836d64775e477da7d5dddfb8fd22e6e89
nkhaldi <mnk80898@gmail.com>
2023-04-21 18:44:10 +0300

    added Reforge for dragons

--- SimpleGames/contracts/DragonFarm.sol
+++ SimpleGames/contracts/DragonFarm.sol
@@ -3,26 +3,28 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract DragonFarm {
   struct Dragon {
+    uint id;
     string name;
     uint dna;
   }
 
   Dragon[] public dragons;
+  mapping (uint => address) public DragonOwner;
+  mapping (address => uint) ownerDragons;
 
-  event NewDragon(string name, uint dna);
+  event NewDragon(uint id, string name, uint dna);
 
   function GenerateRandomDna(string memory _str) private pure returns (uint) {
     uint rand = uint(keccak256((abi.encode(_str))));
     return rand % (10 ** 16);
   }
 
-  function AddDragon(string memory _name, uint _dna) private {
-    dragons.push(Dragon(_name, _dna));
-    emit NewDragon(_name, _dna);
-  }
-
-  function CreateDragon(string memory _name) public {
-    uint randDna = GenerateRandomDna(_name);
-    AddDragon(_name, randDna);
+  function CreateDragon(string memory name) public {
+    uint dna = GenerateRandomDna(name);
+    uint id = ownerDragons[msg.sender];
+    dragons.push(Dragon(id, name, dna));
+    DragonOwner[id] = msg.sender;
+    emit NewDragon(id, name, dna);
+    ownerDragons[msg.sender]++; 
   }
 }
