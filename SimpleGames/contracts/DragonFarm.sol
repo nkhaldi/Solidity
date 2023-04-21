40c9933cd14a67108b739ef1f741330abcd4291a
nkhaldi <mnk80898@gmail.com>
2023-04-21 18:08:44 +0300

    made contract for creating dragons

--- SimpleGames/contracts/DragonFarm.sol
+++ SimpleGames/contracts/DragonFarm.sol
@@ -1,7 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.5.0 <0.9.0;
 
 contract DragonFarm {
-  constructor() public {
+  struct Dragon {
+    string name;
+    uint dna;
+  }
+
+  Dragon[] public dragons;
+
+  event NewDragon(string name, uint dna);
+
+  function GenerateRandomDna(string memory _str) private pure returns (uint) {
+    uint rand = uint(keccak256((abi.encode(_str))));
+    return rand % (10 ** 16);
+  }
+
+  function AddDragon(string memory _name, uint _dna) private {
+    dragons.push(Dragon(_name, _dna));
+    emit NewDragon(_name, _dna);
+  }
+
+  function CreateDragon(string memory _name) public {
+    uint randDna = GenerateRandomDna(_name);
+    AddDragon(_name, randDna);
   }
 }
