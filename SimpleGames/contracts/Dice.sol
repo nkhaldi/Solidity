0f1cabb619b15ffac27546839683d35bc6f0e4f4
nkhaldi <mnk80898@gmail.com>
2023-04-21 17:48:24 +0300

    made Dice payable

--- SimpleGames/contracts/Dice.sol
+++ SimpleGames/contracts/Dice.sol
@@ -1,21 +1,41 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.5.0 <0.9.0;
 
 contract Dice {
+  address public manager;
+  address payable[] public players;
+
+  function CEO() public {
+    manager = msg.sender;
+  }
+
+  function Enter() public payable {
+    require(msg.value > .001 ether, "The bet is too low");
+    players.push(payable(msg.sender));
+  }
+
   function getRandomNumber(uint heuristic) public view returns(uint) {
     uint randDice = (uint(block.timestamp) + heuristic) % 11 + 2;
     return(randDice);
   }
-  function getWinner() public view returns(string memory, uint) {
+
+  function getWinner() public payable restricted returns(string memory, uint) {
     uint player1 = getRandomNumber(0);
     uint player2 = getRandomNumber(6);
 
     if (player1 > player2) {
+      players[0].transfer(address(this).balance);
       return("The winner is player #1", player1);
     } else if (player2 > player1) {
+      players[1].transfer(address(this).balance);
       return("The winner is player #2", player2);
     } else {
       return("Tie", player1);
     }
+  }
+
+  modifier restricted() {
+    require(msg.sender == manager, "Permission denied");
+    _;
   }
 }
